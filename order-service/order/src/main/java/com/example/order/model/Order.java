0874abcb0d29799164024af42c7c907b8306efb7
naren.alp@gmail.com <naren.alp@gmail.com>
2024-06-01 15:11:04 +0530

    Sec Commie 01062024

--- order-service/order/src/main/java/com/example/order/model/Order.java
+++ order-service/order/src/main/java/com/example/order/model/Order.java
@@ -1,6 +1,6 @@
 package com.example.order.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.util.Date;
 
 @Entity
@@ -16,4 +16,53 @@ public class Order {
     private Date orderDate;
 
     // Getters and Setters
+
+    public long getProductId() {
+        return this.productId;
+    }
+
+    public long getUserId() {
+        return this.userId;
+    }
+
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+
+    public Double getTotalPrice() {
+        return this.totalPrice;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public Date getOrderDate() {
+        return this.orderDate;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public void setQuantity(Integer quantity) {
+
+        this.quantity = quantity;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
 }
